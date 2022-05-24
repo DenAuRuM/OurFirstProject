96badb25a2763f93950049fbc18f0e720509c185
Андрей Берляев <piran-ia@mail.ru>
2022-05-24 17:50:50 +0300

    stable

---
+++ GameCafe/Migrations/20220517125631_2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameCafe.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Videogames_VideoGameGenres_VideoGameGenreId",
+                table: "Videogames");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Videogames_VideoGameGenreId",
+                table: "Videogames");
+
+            migrationBuilder.DropColumn(
+                name: "VideoGameGenreId",
+                table: "Videogames");
+
+            migrationBuilder.DropColumn(
+                name: "VideogameId",
+                table: "VideoGameGenres");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VideoGameGenreId",
+                table: "Videogames",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VideogameId",
+                table: "VideoGameGenres",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videogames_VideoGameGenreId",
+                table: "Videogames",
+                column: "VideoGameGenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Videogames_VideoGameGenres_VideoGameGenreId",
+                table: "Videogames",
+                column: "VideoGameGenreId",
+                principalTable: "VideoGameGenres",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ GameCafe/Migrations/20220519142749_5.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameCafe.Migrations
+{
+    public partial class _5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "Tariffs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "Tariffs");
+        }
+    }
+}
